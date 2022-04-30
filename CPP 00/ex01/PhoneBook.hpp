72ff0d3cc0a453a486ecc2e02261d7cb8d29627a
Leo <letumany@c2r5s3.42yerevan.am>
2022-04-30 17:56:06 +0400

    ex01 works! need slight improvement

--- CPP 00/ex01/PhoneBook.hpp
+++ CPP 00/ex01/PhoneBook.hpp
@@ -9,14 +9,30 @@
 #ifndef EX01_PHONEBOOK_HPP
 # define EX01_PHONEBOOK_HPP
 
-#include <iomanip>
+# define WHITE "\001\033[0m\002"
+# define RED "\001\033[1;31m\002"
+# define CYAN "\001\033[1;36m\002"
+# define GREEN "\001\033[1;92m\002"
+# define MAGENTA "\001\033[1;35m\002"
+
+# include <iomanip>
+# include "Contact.hpp"
+# include <string>
 
 class PhoneBook
 {
 public:
-    
+    void    add(void);
+    void    search(void);
+    void    exit(void);
+    void    displayContacts(void);
+    PhoneBook();
+    ~PhoneBook();
 
-
+private:
+    Contact         _contacts[8];
+    unsigned int    _current;
+    unsigned int    _index;
 };
 
 
